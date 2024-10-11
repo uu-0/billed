22fe9267bdd26ce33e9dafad163231f30d2dcaf6
unknown <louisepop14@gmail.com>
2024-10-11 17:55:59 +0200

    erreur 5 : [Ajout de tests unitaires et d'intégration] : composant container/Bills : test d'intégration GET #5

--- Billed-app-FR-Front/src/__tests__/Bills.js
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -5,15 +5,18 @@
 import {screen, waitFor} from "@testing-library/dom"
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
-import { ROUTES_PATH} from "../constants/routes.js";
+import {ROUTES_PATH} from "../constants/routes.js";
 import {localStorageMock} from "../__mocks__/localStorage.js";
+
+import NewBill from "../containers/NewBill.js"
+
+import mockStore from "../__mocks__/store"
 
 import router from "../app/Router.js";
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
     test("Then bill icon in vertical layout should be highlighted", async () => {
-
       Object.defineProperty(window, 'localStorage', { value: localStorageMock })
       window.localStorage.setItem('user', JSON.stringify({
         type: 'Employee'
@@ -27,6 +30,7 @@ describe("Given I am connected as an employee", () => {
       const windowIcon = screen.getByTestId('icon-window')
       expect(windowIcon.classList.contains('active-icon')).toBe(true);
     })
+    
     test("Then bills should be ordered from earliest to latest", () => {
       document.body.innerHTML = BillsUI({ data: bills })
       const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
@@ -34,5 +38,50 @@ describe("Given I am connected as an employee", () => {
       const datesSorted = [...dates].sort(antiChrono)
       expect(dates).toEqual(datesSorted)
     })
-  })
+
+    // //couvrir un maximum de statement
+    // describe("When I click on New Bill button", () => {
+    //   test("It should navigate to NewBill page", () => {
+
+    //     document.body.innerHTML = `<button data-testid="btn-new-bill">New Bill</button>`
+    
+    //     const onNavigate = jest.fn()
+    //     const newBills = new NewBill({ document, onNavigate, store: null, localStorage: window.localStorage })
+  
+    //     const buttonNewBill = screen.getByTestId('btn-new-bill')
+    //     //simule clic sur le bouton
+    //     buttonNewBill.click()
+  
+    //     //vérifie que la navigation a bien eu lieu
+    //     expect(onNavigate).toHaveBeenCalledWith(ROUTES_PATH['NewBill'])
+    //   })
+    // })
+
+
+    //test d'intégration GET
+    test("fetches bills from mock API GET", async () => {
+      //simule authentification user type employé
+      localStorage.setItem("user", JSON.stringify({ type: "Employee", email: "e@e" }));
+
+      //création DOM
+      const root = document.createElement("div")
+      root.setAttribute("id", "root")
+      document.body.append(root)
+
+      //initialise routeur de l'app
+      router()
+
+      //simule navigation user
+      window.onNavigate(ROUTES_PATH.Bills)
+
+      //récupération corps du tableau contenant les bills
+      const tBody  = await screen.getByTestId('tbody')
+
+      expect(tBody).toBeTruthy()
+    })
+
+  }) 
 })
+
+
+
